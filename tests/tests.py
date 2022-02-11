a93d060c36729e803c5b437d67f658dbb4e6df81
MNoun <mnoun@student.bridgew.edu>
2022-02-10 22:55:35 -0500

    Sprint 2: tests

---
+++ tests/tests.py
@@ -0,0 +1,29 @@
+import main
+import pytest
+import requests
+import Secrets
+import sqlite3
+
+
+def test_show_data():  # test 1: retrieves 250
+    imdburl = f"https://imdb-api.com/en/API/Top250TVs/{Secrets.apiKey}"
+    response = requests.get(imdburl)  # gets a response from imDb
+    data = response.json()
+    datalist = data['items']  # makes a list of dict
+    length = len(datalist)  # gets length of dictionary
+    assert length == 250
+
+
+# test 2: database
+def test_database():
+    testdict = {"items": [{"id": "test", "title": "test", "fullTitle": "test",
+                "crew": "test", "year": 0, "imDbRating": 0, "imDbRatingCount": 0}]}  # test data for test database
+    datalist = testdict['items']
+    name = 'test_db.db'
+    connection, cursor = main.db_open(name)
+    main.db_setup(cursor)  # database setup function
+    main.db_populate_top250(connection, cursor, datalist)  # database populate function, using test data
+    cursor.execute("SELECT title FROM show_data")  # selects test title and compares it to correct title
+    res = cursor.fetchall()
+    result = res[0]
+    assert result[0] == "test"
